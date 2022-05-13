d5bcb7a5e462637e97ec4c8a5f0bd4ed94aecd13
aselwilliams <aselwilliams22@gmail.com>
2022-05-13 12:14:00 -0400

    play again btn enabled

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 //place X or O on the cells
 //check the pattern for winner
 //check for tie
+//when someone wins reset game
 
 const cells = document.querySelectorAll('.cell');
+const result = document.querySelector('.result');
+const gameOver = document.querySelector('.game-over');
+const resetBtn = document.querySelector('.reset')
 
 let player1 = true;
 let count=0;
@@ -16,7 +20,7 @@ startGame()
 
 function executeGame(){
     this.innerText = player1 ? 'X' : 'O';
-    player1 = !player1 
+    
     count++; 
 
     let val1 = cells[0].innerText;
@@ -39,12 +43,23 @@ function executeGame(){
         (val1!=='' && val1===val5 && val5===val9) ||
         (val3!=='' && val3===val5 && val5===val7) 
         
-
-
         ){
+            gameOver.style.display='flex';
+            result.innerText = player1 ? 'Player X Won' : 'Player O Won';
         return;
     }
     if(count===9){
         console.log('tie')
     }
+    player1 = !player1 
+}
+
+resetBtn.addEventListener('click', resetGame)
+
+function resetGame(){
+    gameOver.style.display='none';
+    cells.forEach(cell=>cell.innerText='')
+    startGame()
+    player1=true;
+    count=0;
 }
