2ab0b1b49044602f2d2d68e7e463a5b51898d8de
aselwilliams <aselwilliams22@gmail.com>
2022-05-13 11:43:07 -0400

    winning combinations added

--- main.js
+++ main.js
@@ -16,4 +16,30 @@ startGame()
 function executeGame(){
     this.innerText = player1 ? 'X' : 'O';
     player1 = !player1  
+
+    let val1 = cells[0].innerText;
+    let val2 = cells[1].innerText;
+    let val3 = cells[2].innerText;
+    let val4 = cells[3].innerText;
+    let val5 = cells[4].innerText;
+    let val6 = cells[5].innerText;
+    let val7 = cells[6].innerText;
+    let val8 = cells[7].innerText;
+    let val9 = cells[8].innerText;
+   
+    if (
+        (val1!=='' && val1===val2 && val2===val3) ||
+        (val4!=='' && val4===val5 && val5===val6) ||
+        (val7!=='' && val7===val8 && val8===val9) ||
+        (val1!=='' && val1===val4 && val4===val7) ||
+        (val2!=='' && val2===val5 && val5===val8) ||
+        (val3!=='' && val3===val6 && val6===val9) ||
+        (val1!=='' && val1===val5 && val5===val9) ||
+        (val3!=='' && val3===val5 && val5===val7) 
+        
+
+
+        ){
+        console.log('win')
+    }
 }
